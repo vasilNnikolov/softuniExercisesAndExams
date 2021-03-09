c94d0388b8c5753b8abeb5fb40803157f00e92b5
vasilNnikolov <74076246+vasilNnikolov@users.noreply.github.com>
2021-03-09 09:12:51 +0200

    added solution to problem 8 without bugs

--- pythonFundamentals/exercises/28TextProcessingExercise/problem8.py
+++ pythonFundamentals/exercises/28TextProcessingExercise/problem8.py
@@ -1,23 +1,29 @@
-tokens = input().split(' ')
+# tokens = input().split(' ')
+#
+# tokens = [t for t in tokens if len(t) > 0]
+import re
 
-tokens = [t for t in tokens if t != '']
-
-def isCapital(letter):
-    return ord('A') <= ord(letter) <= ord('Z')
-
+pattern = r'([a-zA-Z][-0-9\s]{1,}[a-zA-Z])'
+tokens = re.findall(pattern, input())
 sum = 0
 for token in tokens:
-    n = int(token[1:-1])
+    number = token[1:-1]
+    number = number.replace(" ", "")
+    n = float(number)
     first_letter, second_letter = token[0], token[-1]
     local_sum = n
-    if isCapital(first_letter):
+    if ord("A") <= ord(first_letter) <= ord("Z"):
         local_sum /= ord(first_letter) - ord('A') + 1
-    else:
+
+    if ord("a") <= ord(first_letter) <= ord("z"):
         local_sum *= ord(first_letter) - ord('a') + 1
-    if isCapital(second_letter):
+
+    if ord("A") <= ord(second_letter) <= ord("Z"):
         local_sum -= ord(second_letter) - ord('A') + 1
-    else:
+
+    if ord("a") <= ord(second_letter) <= ord("z"):
         local_sum += ord(second_letter) - ord('a') + 1
+
     sum += local_sum
 
 print(f'{sum:.2f}')
