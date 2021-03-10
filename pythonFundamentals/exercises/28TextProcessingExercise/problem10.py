63fd11cd94a40e57bddd81fe4c86c62d5d42ea85
Vasil Nikolov <vaskonikolov2003@gmail.com>
2021-03-10 21:45:54 +0200

    added problems 9 and 10, both with bugs

--- pythonFundamentals/exercises/28TextProcessingExercise/problem10.py
+++ pythonFundamentals/exercises/28TextProcessingExercise/problem10.py
@@ -1,10 +1,39 @@
 import re
-tickets = input().split(', ')
+
 winning_chars = ['@', '#', '$', '^']
-winning_regex = [f'{c}']
+jackpot_regex = {c: f"(\{c}){{10}}" for c in winning_chars}
+winning_regex = {c: f"(\{c}{{6,9}})" for c in winning_chars}
+
+tickets = input().split(', ')
+tickets = [t.strip() for t in tickets]
+
 for ticket in tickets:
     if len(ticket) != 20:
-        print(f"Invalid ticket - {ticket}")
+        print('invalid ticket')
         continue
-    left_half, right_half = ticket[0:10], ticket[10:20]
+    else:
+        is_winning = False
+        left_half, right_half = ticket[0:10], ticket[10:20]
+
+        for c in winning_chars:
+            if bool(re.search(jackpot_regex[c], left_half)) and bool(re.search(jackpot_regex[c], right_half)):
+                # jackpot
+                print(f"ticket \"{ticket}\" - 10{c} Jackpot!")
+                is_winning = True
+                break
+
+            simple_win = bool(re.search(winning_regex[c], left_half)) and bool(re.search(winning_regex[c], right_half))
+            left_jackpot = bool(re.search(jackpot_regex[c], left_half)) and bool(re.search(winning_regex[c], right_half))
+            right_jackpot = bool(re.search(winning_regex[c], left_half)) and bool(re.search(jackpot_regex[c], right_half))
+
+            if bool(simple_win) or bool(left_jackpot) or bool(right_jackpot):
+                # normal win
+                # print(f"ticket \"{ticket}\" - {max(len(re.findall(winning_regex[c], left_half)[0]), len(re.findall(winning_regex[c], right_half)[0]))}{c}")
+                if len(re.findall(winning_regex[c], left_half)[0]) == len(re.findall(winning_regex[c], right_half)[0]):
+                    print(f"ticket \"{ticket}\" - {len(re.findall(winning_regex[c], left_half)[0])}{c}")
+                    is_winning = True
+                    break
+
+        if not is_winning:
+            print(f"ticket \"{ticket}\" - no match")
 
--- pythonFundamentals/exercises/28TextProcessingExercise/problem9.py
+++ pythonFundamentals/exercises/28TextProcessingExercise/problem9.py
@@ -1,13 +1,14 @@
 
-
 text = input()
 i = 0
 tokens = [] # list of tuples of the form (substring, number of uses)
 while i < len(text):
     token = ["", 0]
+    i_start = i
     while not text[i].isdigit():
-        token[0] += text[i].upper()
         i += 1
+    token[0] = text[i_start:i]
+    token[0] = token[0].upper()
     while text[i].isdigit():
         token[1] = 10*token[1] + int(text[i])
         i += 1
@@ -16,9 +17,11 @@ while i < len(text):
     tokens.append(tuple(token))
 
 used_symbols = [] # list of used symbols
-output = ""
+output = []
 for token in tokens:
-    output += token[0]*token[1]
+    output.extend(list(token[0]*token[1]))
+
+output = "".join(output)
 
 for c in output:
     if c not in used_symbols:
