6b3c1d239fb27b64ce51c310281ed7155fffb1f8
Vasil Nikolov <vaskonikolov2003@gmail.com>
2021-03-11 16:00:28 +0200

    added problems 9 and 10 correct solutions

--- pythonFundamentals/exercises/28TextProcessingExercise/problem10.py
+++ pythonFundamentals/exercises/28TextProcessingExercise/problem10.py
@@ -1,39 +1,37 @@
-import re
+tickets = [t.strip() for t in input().split(", ")]
 
-winning_chars = ['@', '#', '$', '^']
-jackpot_regex = {c: f"(\{c}){{10}}" for c in winning_chars}
-winning_regex = {c: f"(\{c}{{6,9}})" for c in winning_chars}
-
-tickets = input().split(', ')
-tickets = [t.strip() for t in tickets]
-
-for ticket in tickets:
+def is_winning(ticket):
     if len(ticket) != 20:
-        print('invalid ticket')
-        continue
+        return ("invalid ticket", 0)
+
     else:
-        is_winning = False
         left_half, right_half = ticket[0:10], ticket[10:20]
 
-        for c in winning_chars:
-            if bool(re.search(jackpot_regex[c], left_half)) and bool(re.search(jackpot_regex[c], right_half)):
-                # jackpot
-                print(f"ticket \"{ticket}\" - 10{c} Jackpot!")
-                is_winning = True
-                break
+        winning_chars = "$@#^"
+        for winning_char in winning_chars:
+            if winning_char*6 in left_half and winning_char*6 in right_half:
+                best_count = 6
+                for n in range(6, 11):
+                    if winning_char*n in left_half and winning_char*n in right_half:
+                        best_count = n
+                    else:
+                        break
+                return (winning_char, best_count)
 
-            simple_win = bool(re.search(winning_regex[c], left_half)) and bool(re.search(winning_regex[c], right_half))
-            left_jackpot = bool(re.search(jackpot_regex[c], left_half)) and bool(re.search(winning_regex[c], right_half))
-            right_jackpot = bool(re.search(winning_regex[c], left_half)) and bool(re.search(jackpot_regex[c], right_half))
+        return ("no match", 0)
 
-            if bool(simple_win) or bool(left_jackpot) or bool(right_jackpot):
-                # normal win
-                # print(f"ticket \"{ticket}\" - {max(len(re.findall(winning_regex[c], left_half)[0]), len(re.findall(winning_regex[c], right_half)[0]))}{c}")
-                if len(re.findall(winning_regex[c], left_half)[0]) == len(re.findall(winning_regex[c], right_half)[0]):
-                    print(f"ticket \"{ticket}\" - {len(re.findall(winning_regex[c], left_half)[0])}{c}")
-                    is_winning = True
-                    break
 
-        if not is_winning:
-            print(f"ticket \"{ticket}\" - no match")
+for ticket in tickets:
+    tokens = is_winning(ticket)
+    if tokens[0] == "invalid ticket":
+        print("invalid ticket")
 
+    elif tokens[0] == "no match":
+        print(f"ticket \"{ticket}\" - no match")
+
+    else:
+        if 6 <= tokens[1] <= 9:
+            print(f"ticket \"{ticket}\" - {tokens[1]}{tokens[0]}")
+
+        if tokens[1] == 10:
+            print(f"ticket \"{ticket}\" - 10{tokens[0]} Jackpot!")
--- pythonFundamentals/exercises/28TextProcessingExercise/problem9.py
+++ pythonFundamentals/exercises/28TextProcessingExercise/problem9.py
@@ -1,31 +1,22 @@
-
 text = input()
-i = 0
-tokens = [] # list of tuples of the form (substring, number of uses)
-while i < len(text):
-    token = ["", 0]
-    i_start = i
-    while not text[i].isdigit():
-        i += 1
-    token[0] = text[i_start:i]
-    token[0] = token[0].upper()
-    while text[i].isdigit():
-        token[1] = 10*token[1] + int(text[i])
-        i += 1
-        if i >= len(text):
-            break
-    tokens.append(tuple(token))
+in_number = False
+number = ""
+pattern = ""
+output = ""
 
-used_symbols = [] # list of used symbols
-output = []
-for token in tokens:
-    output.extend(list(token[0]*token[1]))
+for index, c in enumerate(text):
+    if c.isdigit():
+        if not in_number:
+            number = ""
+            in_number = True
+        number += c
+    if not c.isdigit() or index == len(text) - 1:
+        if in_number:
+            output += pattern * int(number)
+            in_number = False
+            pattern = ""
+        pattern += c.upper()
 
-output = "".join(output)
 
-for c in output:
-    if c not in used_symbols:
-        used_symbols.append(c)
-
-print(f'Unique symbols used: {len(used_symbols)}')
-print(output.replace("\n", " "))
+print(f"Unique symbols used: {len(set(output))}")
+print(output)
