4ed97b2c8a3426323f314d9cbffedd6ffbfa4a21
Vasil Nikolov <vaskonikolov2003@gmail.com>
2021-06-03 22:29:42 +0300

    solutions to all non-star problems

--- pythonAdvanced/exercises/multidimensionalLists/problem6.py
+++ pythonAdvanced/exercises/multidimensionalLists/problem6.py
@@ -1,46 +1,47 @@
+attackable_positions = [(-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
+
 n = int(input())
 
 board = []
 
+def is_on_board(x, y):
+    if 0 <= x < n and 0 <= y < n:
+        return True
+    return False
+
 for _ in range(n):
     board.append([c for c in input()])
 
-number_of_attacks = {} # a dictionary with keys a tuple (row, col) of a knight, and values the number of knights it attacks
-
-def get_number_of_attacks(y, x, board):
-    total_attacks = 0
-    for i in range(-2, 3):
-        for j in range(-2, 3):
-            if (abs(i) == 2 and abs(j) == 1) or (abs(i) == 1 and abs(j) == 2):
-                if 0 <= y + i < len(board) and 0 <= x + j < len(board):
-                    if board[y + i][x + j] == "K":
-                        total_attacks += 1
-
-    return total_attacks
+attacking = {} # key is (row, col) of knight, value is a list of all coordinates which it attacks and there are knights
+for y in range(n):
+    for x in range(n):
+        if board[y][x] == "K":
+            attacking[(y, x)] = []
 
 
 for y in range(n):
     for x in range(n):
         if board[y][x] == "K":
-            number_of_attacks[(y, x)] = get_number_of_attacks(y, x, board)
+            for p in attackable_positions:
+                x1, y1 = x + p[0], y + p[1]
+                if is_on_board(x1, y1):
+                    if board[y1][x1] == "K":
+                        attacking[(y, x)].append((y1, x1))
 
-# sort the array
-number_of_attacks = dict(sorted(number_of_attacks.items(), key=lambda entry: entry[1], reverse=True))
+removed_knights = 0
+while sum([len(i) for i in attacking.values()]) != 0:
+    # get key which attacks the highest number of other knights
+    most_angry_knight = max(attacking.items(), key=lambda item: len(item[1]))
+    attacking.pop(most_angry_knight[0])
+    for knight in attacking:
+        if most_angry_knight[0] in attacking[knight]:
+            attacking[knight].remove(most_angry_knight[0])
 
-removed = 0
-while sum(number_of_attacks.values()) > 0:
-    first_key = number_of_attacks.keys()[0]
-    board[first_key[0]][first_key[1]] = "0"
-    removed += 1
-    # recalculate dict
-    number_of_attacks = {}  # a dictionary with keys a tuple (row, col) of a knight, and values the number of knights it attacks
-    for y in range(n):
-        for x in range(n):
-            if board[y][x] == "K":
-                number_of_attacks[(y, x)] = get_number_of_attacks(y, x, board)
-
-    # sort the array
-    number_of_attacks = dict(sorted(number_of_attacks.items(), key=lambda entry: entry[1], reverse=True))
+    removed_knights += 1
 
 
-print(removed)
+print(removed_knights)
+
+
+
+
